bcb722ab5f59595900b788097c76702abc9f4f06
Hande-taskiran <handetaskiranht@gmail.com>
2024-09-10 14:21:05 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ToastContainer />
+      </div>
+    </>
+  );
+}
+
+export default App;
