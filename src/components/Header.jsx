2205471febc87510fda23bd42effef2ffa9b8018
Hande-taskiran <handetaskiranht@gmail.com>
2024-09-12 17:10:16 +0300

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
-import { Form, FormGroup, Label, Input } from 'reactstrap';
+import { Form, FormGroup, Input } from 'reactstrap';
 
 const Header = ({ darkMode, setDarkMode }) => {
   return (
-    <header className="bg-purple-700 dark:bg-black text-white dark:text-white p-10 flex flex-col items-center">
-      <div className="flex items-center ml-auto gap-4">
-        <button className="px-4 py-2 bg-purple-700 text-white rounded-full">
+    <header className="relative bg-blue-700 dark:bg-black text-white dark:text-white p-10 flex items-center justify-between h-62">
+      <div className="flex flex-col items-start text-green-300 z-10 self-start pl-14">
+        <p className="text-1xl font-bold mb-6">Hande Taşkıran</p>
+        <p className="text-2xl mb-6">I am a Frontend Developer...</p>
+        <p className="text-white mb-4 text-left max-w-md w-auto">
+          ...who likes to craft solid and scalable frontend products with great
+          user experiences.
+        </p>
+
+        <div className="flex gap-4">
+          <button className="px-4 py-2 bg-white text-blue-700">Github</button>
+          <button className="px-4 py-2 bg-white text-blue-700">LinkedIn</button>
+        </div>
+      </div>
+
+      <div className="absolute top-0 right-1/3 p-4 z-10">
+        <button className="px-4 py-2 bg-blue-700 text-green-300 rounded-full">
           Türkçeye Geç
         </button>
-        <Form className="flex items-center gap-2">
+      </div>
+
+      <div className="absolute top-0 right-0 h-full w-1/3 bg-gradient-to-r from-green-300 to-green-500 z-0"></div>
+
+      <div className="absolute top-0 right-0 p-4 z-10">
+        <Form className="flex items-center">
           <FormGroup switch>
-            <span>{darkMode ? 'Light Mode' : 'Dark Mode'}</span>
+            <span className="mr-2">
+              {darkMode ? 'Light Mode' : 'Dark Mode'}
+            </span>
             <Input
               type="switch"
               role="switch"
@@ -20,20 +41,6 @@ const Header = ({ darkMode, setDarkMode }) => {
             />
           </FormGroup>
         </Form>
-      </div>
-      <div className="flex flex-col items-center mr-auto ">
-        <p className="text-1xl font-bold mb-4">Hande Taşkıran</p>
-        <p className="text-2xl mb-6">I am a Frontend Developer...</p>
-        <p>
-          ...who likes to craft solid and scalable frontend products with great
-          user experiences.
-        </p>
-        <div className="flex gap-4">
-          <button className="px-4 py-2 bg-white text-purple-700">Github</button>
-          <button className="px-4 py-2 bg-white text-purple-700">
-            LinkedIn
-          </button>
-        </div>
       </div>
     </header>
   );
